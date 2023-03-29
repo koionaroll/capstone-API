a3f4487b49fd36d12a4b7d4fed07453c6963b91d
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-29 15:51:32 -0700

    aded routes

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const decksRoute = require("./routes/decksRoute")
+const singlesRoute = require("./routes/singlesRoute")
+
+require("dotenv").config()
+const port = process.env.PORT
+
+// middleware parses request body as JSON, Need this for access to req.body for posts
+app.use(express.json()); 
+app.use(express.static('public'));
+app.use(cors())
+
+app.use('/decks', decksRoute);
+app.use('/singles', singlesRoute);
+
+
+
+app.listen(port, () => {
+  console.log(`Express demo listening on port ${port}`);
+});
