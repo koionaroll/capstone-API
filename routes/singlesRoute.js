9b006e10c541589e4fc353020e6b2b28e11c138c
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-31 21:19:22 -0700

    added routes

--- routes/singlesRoute.js
+++ routes/singlesRoute.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const mtg = require("mtgsdk");
+const axios = require("axios");
+const SCRYFALL_URL = "https://api.scryfall.com/cards/named?fuzzy="
 
-// Filter Cards
-router.get("/test", (req, res) => {
-  mtg.card.all({ name: "Experiment One" }).on("data", (card) => {
-    res.json({name:card.name, image:card.imageUrl});
+const cardname = "black lotus"
+
+// router.get("/test", (req, res) => {
+
+//   axios.get(`${SCRYFALL_URL}${cardname}`).then((response) => {
+//     res.json({
+//       id:response.data.id, 
+//       name:response.data.name, 
+//       img:response.data.image_uris.png
+//     });
+//   });
+// });
+
+router.get("/:value", (req, res) => {
+  const value = req.params.value;
+  console.log(value)
+  axios.get(`${SCRYFALL_URL}${value}}`).then((response) => {
+    res.json({
+      id:response.data.id, 
+      name:response.data.name, 
+      img:response.data.image_uris.png
+    });
   });
 });
 
