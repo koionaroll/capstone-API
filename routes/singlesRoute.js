86f9f4c0213a8f1b413ed9046285dc03578b2b18
Khoi Tran <tranvankhoi2002@gmail.com>
2023-04-03 14:58:38 -0700

    fixed routes

--- routes/singlesRoute.js
+++ routes/singlesRoute.js
@@ -1,30 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const SCRYFALL_URL = "https://api.scryfall.com/cards/named?fuzzy="
-
-const cardname = "black lotus"
-
-// router.get("/test", (req, res) => {
-
-//   axios.get(`${SCRYFALL_URL}${cardname}`).then((response) => {
-//     res.json({
-//       id:response.data.id, 
-//       name:response.data.name, 
-//       img:response.data.image_uris.png
-//     });
-//   });
-// });
+const SCRYFALL_URL = "https://api.scryfall.com/cards/named?fuzzy=";
 
 router.get("/:value", (req, res) => {
   const value = req.params.value;
-  console.log(value)
   axios.get(`${SCRYFALL_URL}${value}}`).then((response) => {
     res.json({
-      id:response.data.id, 
-      name:response.data.name, 
-      img:response.data.image_uris.png
-    });
+      id: response.data.id,
+      name: response.data.name,
+      img: response.data.image_uris.png,
+    })
+    // .catch((err)=>{
+    //   res.send("Error: ",err)
+    // });
   });
 });
 
